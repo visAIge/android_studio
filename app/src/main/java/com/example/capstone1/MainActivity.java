0da970f261dd50247788702f49beba4e76b7fa1a
Sun Young Bae <tjsdud6677@gmail.com>
2023-04-13 23:09:00 +0900

    main side menu update

--- app/src/main/java/com/example/capstone1/MainActivity.java
+++ app/src/main/java/com/example/capstone1/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.capstone1;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.ContextMenu;
@@ -10,6 +15,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
+import com.google.android.material.navigation.NavigationView;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -19,17 +25,51 @@ public class MainActivity extends AppCompatActivity {
     private Button btn_QR;
     private Button btn_log;
     private ImageView imageView;
-
+    private Toolbar toolbar;
+    private NavigationView navigationView;
     String login_user_id;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
         Intent login_intent = getIntent();
         login_user_id = login_intent.getExtras().getString("login_user_id");
 
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        navigationView = (NavigationView)findViewById(R.id.nav);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+                int id = menuItem.getItemId();
+
+                if (id == R.id.menu_pwd){
+                    Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                    startActivity(intent);
+                }else if(id == R.id.menu_otp){
+                    Intent intent = new Intent(MainActivity.this, MainActivity3.class);
+                    intent.putExtra("login_user_id", login_user_id);
+                    startActivity(intent);
+                }else if(id == R.id.menu_lock){
+                    Intent intent = new Intent(MainActivity.this, MainActivity5.class);
+                    startActivity(intent); //실제 화면 이동
+                }else if(id == R.id.menu_create_qr){
+                    Intent intent = new Intent(MainActivity.this, MainActivity4.class);
+                    startActivity(intent);
+                }else if(id == R.id.menu_qr){ // 생성된 qr 확인
+                    Intent intent = new Intent(MainActivity.this, CreateQR.class);
+                    intent.putExtra("login_user_id", login_user_id);
+                    startActivity(intent);
+                }
+                DrawerLayout drawer = findViewById(R.id.drawer_layout);
+                drawer.closeDrawer(GravityCompat.START);
+                return true;
+            }
+        });
 
         btn_password = findViewById(R.id.input_password);
         btn_password.setOnClickListener(new View.OnClickListener() {
@@ -63,8 +103,6 @@ public class MainActivity extends AppCompatActivity {
         btn_log.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-//                Intent intent = new Intent(MainActivity.this, MainActivity5.class);
-//                startActivity(intent); //실제 화면 이동
                 Intent intent = new Intent(MainActivity.this, CreateQR.class);
                 intent.putExtra("login_user_id", login_user_id);
                 startActivity(intent); //실제 화면 이동
@@ -73,26 +111,5 @@ public class MainActivity extends AppCompatActivity {
 
         imageView = (ImageView) findViewById(R.id.main_image_view);
         registerForContextMenu(imageView);
-    }
-
-    @Override
-    public void onCreateContextMenu(ContextMenu menu, View v,
-                                    ContextMenu.ContextMenuInfo menuInfo)
-    {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.main_menu, menu);
-    }
-
-    public boolean onContextItemSelected(MenuItem item)
-    {
-        switch(item.getItemId())
-        {
-            case R.id.user_info_item:
-                Intent intent = new Intent(MainActivity.this, user_info.class);
-                intent.putExtra("login_user_id", login_user_id);
-                startActivity(intent); //실제 화면 이동
-                break;
-        }
-        return super.onContextItemSelected(item);
     }
 }
